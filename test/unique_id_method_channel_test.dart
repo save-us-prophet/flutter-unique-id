636f12274453f1acb4185152d5a48fbf9a26570f
SANGWOO PARK <cyberprophet@saveus.it>
2024-07-19 18:27:51 +0900

    build android

--- test/unique_id_method_channel_test.dart
+++ test/unique_id_method_channel_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
+import 'package:flutter_core_unique_id/unique_id_method_channel.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:unique_id/unique_id_method_channel.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -9,7 +9,8 @@ void main() {
   const MethodChannel channel = MethodChannel('unique_id');
 
   setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(
       channel,
       (MethodCall methodCall) async {
         return '42';
@@ -18,7 +19,8 @@ void main() {
   });
 
   tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, null);
   });
 
   test('getPlatformVersion', () async {
--- test/unique_id_test.dart
+++ test/unique_id_test.dart
@@ -1,7 +1,7 @@
+import 'package:flutter_core_unique_id/unique_id.dart';
+import 'package:flutter_core_unique_id/unique_id_method_channel.dart';
+import 'package:flutter_core_unique_id/unique_id_platform_interface.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:unique_id/unique_id.dart';
-import 'package:unique_id/unique_id_platform_interface.dart';
-import 'package:unique_id/unique_id_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
 class MockUniqueIdPlatform
